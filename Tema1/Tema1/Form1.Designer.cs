2eb08dad464dd6ff5cfb91d62f35368058ca7181
Vl4dCr1st14n <47784639+Vl4dCr1st14n@users.noreply.github.com>
2021-05-16 19:23:08 +0300

    Add files via upload

---
+++ Tema1/Tema1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace Tema1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNume1 = new System.Windows.Forms.TextBox();
+            this.txtAni1 = new System.Windows.Forms.TextBox();
+            this.txtAni2 = new System.Windows.Forms.TextBox();
+            this.txtNume2 = new System.Windows.Forms.TextBox();
+            this.btnRez = new System.Windows.Forms.Button();
+            this.lblNume1 = new System.Windows.Forms.Label();
+            this.lblNume2 = new System.Windows.Forms.Label();
+            this.lblAni1 = new System.Windows.Forms.Label();
+            this.lblAni2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.lblNrProblema = new System.Windows.Forms.Label();
+            this.btnCerinta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNume1
+            // 
+            this.txtNume1.Location = new System.Drawing.Point(242, 76);
+            this.txtNume1.Name = "txtNume1";
+            this.txtNume1.Size = new System.Drawing.Size(100, 20);
+            this.txtNume1.TabIndex = 0;
+            this.txtNume1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNume1_KeyPress);
+            // 
+            // txtAni1
+            // 
+            this.txtAni1.Location = new System.Drawing.Point(242, 117);
+            this.txtAni1.Name = "txtAni1";
+            this.txtAni1.Size = new System.Drawing.Size(100, 20);
+            this.txtAni1.TabIndex = 1;
+            this.txtAni1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAni1_KeyPress);
+            // 
+            // txtAni2
+            // 
+            this.txtAni2.Location = new System.Drawing.Point(508, 117);
+            this.txtAni2.Name = "txtAni2";
+            this.txtAni2.Size = new System.Drawing.Size(100, 20);
+            this.txtAni2.TabIndex = 2;
+            this.txtAni2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAni1_KeyPress);
+            // 
+            // txtNume2
+            // 
+            this.txtNume2.Location = new System.Drawing.Point(508, 76);
+            this.txtNume2.Name = "txtNume2";
+            this.txtNume2.Size = new System.Drawing.Size(100, 20);
+            this.txtNume2.TabIndex = 4;
+            this.txtNume2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNume1_KeyPress);
+            // 
+            // btnRez
+            // 
+            this.btnRez.Location = new System.Drawing.Point(357, 154);
+            this.btnRez.Name = "btnRez";
+            this.btnRez.Size = new System.Drawing.Size(75, 23);
+            this.btnRez.TabIndex = 5;
+            this.btnRez.Text = "Rezultat";
+            this.btnRez.UseVisualStyleBackColor = true;
+            this.btnRez.Click += new System.EventHandler(this.btnRez_Click);
+            // 
+            // lblNume1
+            // 
+            this.lblNume1.AutoSize = true;
+            this.lblNume1.Location = new System.Drawing.Point(172, 79);
+            this.lblNume1.Name = "lblNume1";
+            this.lblNume1.Size = new System.Drawing.Size(67, 13);
+            this.lblNume1.TabIndex = 6;
+            this.lblNume1.Text = "Nume Copil :";
+            // 
+            // lblNume2
+            // 
+            this.lblNume2.AutoSize = true;
+            this.lblNume2.Location = new System.Drawing.Point(435, 79);
+            this.lblNume2.Name = "lblNume2";
+            this.lblNume2.Size = new System.Drawing.Size(67, 13);
+            this.lblNume2.TabIndex = 7;
+            this.lblNume2.Text = "Nume Copil :";
+            // 
+            // lblAni1
+            // 
+            this.lblAni1.AutoSize = true;
+            this.lblAni1.Location = new System.Drawing.Point(208, 120);
+            this.lblAni1.Name = "lblAni1";
+            this.lblAni1.Size = new System.Drawing.Size(28, 13);
+            this.lblAni1.TabIndex = 8;
+            this.lblAni1.Text = "Ani :";
+            // 
+            // lblAni2
+            // 
+            this.lblAni2.AutoSize = true;
+            this.lblAni2.Location = new System.Drawing.Point(477, 120);
+            this.lblAni2.Name = "lblAni2";
+            this.lblAni2.Size = new System.Drawing.Size(28, 13);
+            this.lblAni2.TabIndex = 9;
+            this.lblAni2.Text = "Ani :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(336, 221);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 104);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(571, 165);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(195, 260);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(61, 165);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(206, 247);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // lblNrProblema
+            // 
+            this.lblNrProblema.AutoSize = true;
+            this.lblNrProblema.Location = new System.Drawing.Point(41, 23);
+            this.lblNrProblema.Name = "lblNrProblema";
+            this.lblNrProblema.Size = new System.Drawing.Size(113, 13);
+            this.lblNrProblema.TabIndex = 13;
+            this.lblNrProblema.Text = "Problema #833 PbInfo";
+            // 
+            // btnCerinta
+            // 
+            this.btnCerinta.Location = new System.Drawing.Point(357, 28);
+            this.btnCerinta.Name = "btnCerinta";
+            this.btnCerinta.Size = new System.Drawing.Size(75, 23);
+            this.btnCerinta.TabIndex = 14;
+            this.btnCerinta.Text = "Cerinta";
+            this.btnCerinta.UseVisualStyleBackColor = true;
+            this.btnCerinta.Click += new System.EventHandler(this.btnCerinta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerinta);
+            this.Controls.Add(this.lblNrProblema);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblAni2);
+            this.Controls.Add(this.lblAni1);
+            this.Controls.Add(this.lblNume2);
+            this.Controls.Add(this.lblNume1);
+            this.Controls.Add(this.btnRez);
+            this.Controls.Add(this.txtNume2);
+            this.Controls.Add(this.txtAni2);
+            this.Controls.Add(this.txtAni1);
+            this.Controls.Add(this.txtNume1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNume1;
+        private System.Windows.Forms.TextBox txtAni1;
+        private System.Windows.Forms.TextBox txtAni2;
+        private System.Windows.Forms.TextBox txtNume2;
+        private System.Windows.Forms.Button btnRez;
+        private System.Windows.Forms.Label lblNume1;
+        private System.Windows.Forms.Label lblNume2;
+        private System.Windows.Forms.Label lblAni1;
+        private System.Windows.Forms.Label lblAni2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label lblNrProblema;
+        private System.Windows.Forms.Button btnCerinta;
+    }
+}
+
---
+++ Tema1/Tema1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAni1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 46 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNume1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsLetter(e.KeyChar) && e.KeyChar!=46 && e.KeyChar!=8 && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }   
+
+        private void btnRez_Click(object sender, EventArgs e)
+        {
+            int aniC1;
+            int aniC2;
+
+            Int32.TryParse(txtAni1.Text, out aniC1);
+            Int32.TryParse(txtAni2.Text, out aniC2);
+
+            if(txtAni1.Text=="" || txtAni2.Text=="" || txtNume1.Text=="" || txtNume2.Text == "")
+            {
+                MessageBox.Show("Scrie in toate casutele!");
+            }
+            else
+            {
+                if(aniC1>aniC2)
+                {
+                    MessageBox.Show(txtNume1.Text + " are cu " + (aniC1 - aniC2) + " mai multi ani.");
+                }
+
+                if (aniC2 > aniC1)
+                {
+                    MessageBox.Show(txtNume2.Text + " are cu " + (aniC2 - aniC1) + " mai multi ani.");
+                }
+                if(aniC2==aniC1)
+                {
+                    MessageBox.Show("Cei doi au varsta egala.");
+                }
+            }
+        }
+
+        private void btnCerinta_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programul va calcula diferența de vârstă x și va afișa pe ecran <<Primul copil este mai mare cu x ani>> dacă primul copil are cea mai mare vârstă, <<Al doilea copil este mai mare cu x ani>> dacă al doilea copil este cel mai în vârstă sau <<Copiii au varste egale>> dacă x=0.");
+        }
+    }
+}
